8f35e5044cd927106cfa9bc7725635ac6fb0eef9
alammd0 <sonualam96314@gmail.com>
2026-01-21 16:27:46 +0530

    fixed type erro

--- app/dashboard/blog/page.tsx
+++ app/dashboard/blog/page.tsx
@@ -1,9 +1,10 @@
 import { decoded } from "@/utils/decoded";
 import BlogClient from "@/components/common/blog/BlogClient";
+import { UserType } from "@/utils/type";
 
 export default async function BlogPage() {
   
-  const User = await decoded();
+  const User = await decoded() as UserType;
 
   return <BlogClient user={User} />;
 }
